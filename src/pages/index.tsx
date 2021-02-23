3064c1bd18c346bb57ff258dc9437f3d308cfbb2
Lusca Lima <lusca.lima@outlook.com>
2021-02-23 09:35:27 -0300

    colocando componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 // Components
 import ExperienceBar from "../components/ExperienceBar";
 import Container from "../components/Container";
+import Profile from "../components/Profile";
+
+// Style
+import style from "../style/scss/pages/home.style.module.scss";
+import CompleteChallenges from "../components/CompleteChallenges";
 
 export default function Home() {
   return (
     <Container>
       <ExperienceBar />
+      <section className={style.home}>
+        <div className={style.leftContainer}>
+          <Profile />
+          <CompleteChallenges />
+        </div>
+        <div className={style.rightContainer}></div>
+      </section>
     </Container>
   );
 }
